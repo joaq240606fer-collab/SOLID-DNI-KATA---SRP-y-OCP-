db3d7baf87f8de54d9a492e5590a4d689671885d
joaq240606fer-collab <joaq240606fer@gamil.com>
2026-02-09 13:13:19 +0100

    feat(Añadir codigo): :sparkles: Añadir codigo get tabla y get letra
    Añadimos las funciones get tabla y get letra

---
+++ src/tablaAsignacion.py
@@ -0,0 +1,39 @@
+class TablaAsignacion():
+
+    # podemos utilizar este ejercicio para sobrecarga
+    # de operaciones sobre listas
+    def __init__(self):
+        self.tabla = [
+            "T",
+            "R",
+            "W",
+            "A",
+            "G",
+            "M",
+            "Y",
+            "F",
+            "P",
+            "D",
+            "X",
+            "B",
+            "N",
+            "J",
+            "Z",
+            "S",
+            "Q",
+            "V",
+            "H",
+            "L",
+            "C",
+            "K",
+            "E",
+        ]
+
+    def getTabla(self):
+        return self.tabla
+
+    def getLetra(self, posicion):
+        try:
+            return self.tabla[posicion]
+        except IndexError:
+            return "Posicion letra fuera de rango"
--- test/test_tabla_asignacion.py
+++ test/test_tabla_asignacion.py
@@ -11,7 +11,7 @@ def tablaAsignacion():
 
 def test_getTabla(tabla):
 
-    assert tabla.getTabla() == [
+    assert tabla.getTabla() ==[
         "T",
         "R",
         "W",
