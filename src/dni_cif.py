0355b444fee2b125afc1875de640aba056ccf0f6
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-02-09 14:03:46 +0100

    feat: Pasar test de setters

--- src/dni_cif.py
+++ src/dni_cif.py
@@ -1,32 +1,25 @@
 class Dni():
-    def __init__(self, dni=""):
-        self.dni = dni
-        self.numero = self._obtenerNumero()
-        self.letra = self._obtenerNumero()
+    def __init__(self, cadena=""):
+        self.dni = cadena
+        self.numeroSano = False
+        self.letra = False
+
+    def setDni(self, cadena):
+        self.dni = cadena
 
     def getDni(self):
         return self.dni
-    
-    def getNumeroSano(self):
-        try:
-            return self.numero is int
-        except:
-            return False
 
-    def getLetraSana(self):
-        try:
-            return self.letra is str
-        except:
-            return False
+    def getNumeroSano(self):
+        return self.numeroSano
     
-    def _obtenerNumero(self):
-        try:
-            return self.dni[:9]
-        except:
-            return ''
-        
-    def _obtenerLetra(self):
-        try:
-            return list(self.dni)[-1]
-        except:
-            return ''
+    def getLetraSana(self):
+        return self.letra
+    
+    def checkDni(self):
+
+    def checkLetra(self):
+
+
+    def checkCIF(self):
+        return self.checkDni() and self.checkLetra()
